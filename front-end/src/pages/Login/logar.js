81e3391d904ceb5c5edfa642f509f5b1fc254c3f
guilhermeandrade02 <guilhermeandrade02@hotmail.com>
2023-06-18 15:51:31 -0300

    validação dos dados para login

--- front-end/src/pages/Login/logar.js
+++ front-end/src/pages/Login/logar.js
@@ -1,56 +1,56 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../Login/login.css';
 
 function Logar() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [senha, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    function handlePasswordChange(event) {
-        setPassword(event.target.value);
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+ //Faz a Busca no banco para ver se existe cadastro e efetua o login
+  async function handleLogin() {
+    try {
+      const response = await axios.get('http://localhost:3000/client/', {
+        email,
+        senha
+      });
+
+      if (response.status === 200) {
+        navigate('/');
+      } else {
+        alert('Email ou senha inválidos');
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    function handleEmailChange(event) {
-        setEmail(event.target.value);
-    }
-
-    function handleLogin() {
-        // login teste cliente
-        if (email === 'cliente@cliente.com' && password === 'senhaCliente') {
-
-            navigate('/');
-            // login teste adm
-        } else if (email === 'admin@admin.com' && password === 'senhaAdmin') {
-
-            navigate('/admin');
-        } else {
-
-            alert('Email ou senha inválidos');
-        }
-    }
-
-    return (
-        <div className="container">
-            <div className="card">
-                <a className="login">Log in</a>
-                <div className="inputBox">
-                    <input type="text" value={email} onChange={handleEmailChange} />
-                    <span className="user">Username</span>
-                </div>
-
-                <div className="inputBox">
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                    <span>Password</span>
-                </div>
-
-                <button className="enter" onClick={handleLogin}>entrar</button>
-
-                <Link to="/">cadastrar</Link>
-            </div>
+  return (
+    <div className="container">
+      <div className="card">
+        <a className="login">Log in</a>
+        <div className="inputBox">
+          <input type="text" value={email} onChange={handleEmailChange} />
+          <span className="user">Username</span>
         </div>
-    );
+        <div className="inputBox">
+          <input type="password" value={senha} onChange={handlePasswordChange} />
+          <span>Password</span>
+        </div>
+        <button className="enter" onClick={handleLogin}>entrar</button>
+        <Link to="/">cadastrar</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Logar;
