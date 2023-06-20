a66d4bae17f563fc88c0f53456dcbf697f2b7c1c
guilhermeandrade02 <guilhermeandrade02@hotmail.com>
2023-06-20 19:49:47 -0300

    atualização

--- back-end/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    codigo: Number,
-    nome: String,
-    descricao: String
-});
-
-module.exports = mongoose.model('categorias', categorySchema);
